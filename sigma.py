f77698ebfe981c75c92352771d0721b7b4893ebe
Israel R. Curbelo <74841063+OnlineDimension@users.noreply.github.com>
2024-11-18 11:35:30 -0500

    added checks and CR calculator
    fixed type in CGJMP (previously f): i + 1 should be i - 1
    added checks for each interval being in [0,n//b] and length being in [1,sigma]
    added function chromatic to compute chromatic number of X
    added function CR to compute competitive ratio 
    cleaned up the print statement to include each interval along with color and finally CR

--- sigma.py
+++ sigma.py
@@ -5,22 +5,35 @@ b=1
 n=100
 sigma=1.5
 
-def f(X,b,n,sigma):
-
-	phi=ceil(b*(1+sigma))
+def CGJMP(X, n=100, b=2, sigma=1.5):
+	# This is an implemenation of the algorithm for Theorem 3 of the 2024 paper by Chybowska-Sokol et al.
+	
+	phi = ceil(b*(1+sigma))
 	S = [0]*n
 	L = [0]*n
 	out = []
 
 	for I in X:
+		# Check that interval has valid length
+		if I[1]-I[0] > sigma:
+			print("The interval", I, "is too long.")
+			return
+		if I[1]-I[0] < 1:
+			print("The interval", I, "is too short.")
+			return
+		if I[0] < 0 or I[1]> (n/b)//1:
+			print("The interval", I, "is out of bounds.")
+			return
+			
 		#1. Let i be the index of the small block containing the left endpoint of the interval
-		i = (I[0]*b)//1
+		i = int((I[0]*b)//1)
 
 		#2. Let j be the index of the large block containing the left endpoint of the interval such that j = S[i] mod b
-		if S[i]%b == ((I[0]*b)//1)%b:
-			j = (I[0]*b)//1
+		# Fancy version: j = (S[i]%b == i%b) + 1
+		if S[i]%b == i%b:
+			j = i
 		else:
-			j = ((I[0]*b)//1)+1
+			j = i - 1
 
 		#3. Assign the interval the color (j mod phi, L[j])
 		out.append((j%phi,L[j]))
@@ -29,9 +42,39 @@ def f(X,b,n,sigma):
 		S[i] += 1
 
 		#5. Increase the large counter by 1
-
 		L[j] += 1
 
 	return out
 
-print(f(X,b,n,sigma))
+def chromatic(X):
+	# Find the chromatic number of X to compute the competitive ratio.
+	M = 0
+	count = 0
+	Endpoints = []
+
+	for I in X:
+		Endpoints.append((X[0],1))
+		Endpoints.append((X[1],-1))
+	Endpoints.sort()
+	
+	for e in Endpoints:
+		count += e[1]
+		M = max(M, count)
+
+	return M
+
+def CR(X, colors):
+	# Compute the competitive ratio
+	bag = {}
+	count = 0
+	for color in colors:
+		if color not in bag:
+			count += 1
+			bag.add(color)
+	return count/chromatic(X)
+
+colors = CGJMP(X)
+for i in range(len(X)):
+	print(X[i], ":",  colors[i])
+R = CR(X, colors)
+print("The competitive ratio is", R)
